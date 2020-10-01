770e1daed212da34474cbcbf416fda0575c8b3e6
dmytroony <dm.onysko@gmail.com>
2020-10-01 16:38:27 +0300

    backToTop button done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 
+// Counting age
 const getCurrentAge = date => ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
 const myAge = getCurrentAge('1989-11-07');
-const currentAge = document.getElementById('currentAge').innerText = myAge;
+document.getElementById('currentAge').innerText = myAge;
+
+
+// Back To Top
+const arrowTop = document.querySelector('.back_to_top');
+
+arrowTop.onclick = function() {
+	window.scrollTo(pageXOffset, 0);
+};
+
+window.addEventListener('scroll', function() {
+	arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+	arrowTop.style.zIndex = '9999';
+});
