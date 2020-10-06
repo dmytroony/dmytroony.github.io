d78128686ae33b98da082332f89ad01b62e76c5f
dmytroony <dm.onysko@gmail.com>
2020-10-06 22:25:36 +0300

    added face-emotions-detection project

--- src/js/main.js
+++ src/js/main.js
@@ -9,31 +9,31 @@ document.getElementById('currentAge').innerText = myAge;
 const arrowTop = document.querySelector('.back_to_top');
 
 arrowTop.onclick = function() {
-	window.scrollTo(pageXOffset, 0);
+  window.scrollTo(pageXOffset, 0);
 };
 
 window.addEventListener('scroll', function() {
-	arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
-	arrowTop.style.zIndex = '9999';
+  arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+  arrowTop.style.zIndex = '9999';
 });
 
 
 // Resume update - upload
 let getResumeDate = (refreshType, year, month, day) => {
-	let refreshDate = `${year}.${month}.${day}`;
+  let refreshDate = `${year}.${month}.${day}`;
 
-	if (refreshType === 'pdf') {
-		document.body.querySelectorAll('.typePdf')
-			.forEach(typePdf => {
-				typePdf.title = `Last upload: ${refreshDate}`
-			});
-	}
-	if (refreshType === 'online') {
-		document.body.querySelectorAll('.typeOnline')
-			.forEach(typeOnline => {
-				typeOnline.title = `Last update: ${refreshDate}`
-			});
-	}
+  if (refreshType === 'pdf') {
+    document.body.querySelectorAll('.typePdf')
+        .forEach(typePdf => {
+          typePdf.title = `Last upload: ${refreshDate}`
+        });
+  }
+  if (refreshType === 'online') {
+    document.body.querySelectorAll('.typeOnline')
+        .forEach(typeOnline => {
+          typeOnline.title = `Last update: ${refreshDate}`
+        });
+  }
 
 
 }
